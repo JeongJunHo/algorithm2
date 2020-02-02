6eaf4f3d644bbaa520976d15e2407a4991f23ce2
정준호 <jjh49470826@gmail.com>
2020-02-02 23:37:37 +0900

    [+] 나무재테크 문제 풀이 및 폴더 이동

--- baekjoon/나무재테크_16235/나무재테크_16235.java
+++ baekjoon/나무재테크_16235/나무재테크_16235.java
@@ -1,19 +1,14 @@
-package algorithm;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 나무재테크_16235 {
 	static class Ground {
-		PriorityQueue<Tree> treeQueue;
-		Queue<Tree> tmpQ;
+		LinkedList<Tree> treeList;
 		int energy;
 		int addEnergy;
 		
@@ -21,8 +16,7 @@ public class 나무재테크_16235 {
 			super();
 			this.energy = energy;
 			this.addEnergy = addEnergy;
-			treeQueue = new PriorityQueue<Tree>();
-			tmpQ = new LinkedList<Tree>();
+			treeList = new LinkedList<Tree>();
 		}
 	}
 	
@@ -77,31 +71,28 @@ public class 나무재테크_16235 {
 			int c = Integer.parseInt(st.nextToken());
 			int age = Integer.parseInt(st.nextToken());
 			Tree tree = new Tree(r, c, age);
-			map[r][c].treeQueue.add(tree);
+			map[r][c].treeList.add(tree);
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				Collections.sort(map[i][j].treeList);
+			}
 		}
 		
 		for (int tc = 0; tc < K; tc++) {
 			//봄
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
-					while (!map[i][j].treeQueue.isEmpty()) {
-						Tree tree = map[i][j].treeQueue.poll();
+					for (Iterator iterator = map[i][j].treeList.iterator(); iterator.hasNext();) {
+						Tree tree = (Tree) iterator.next();
+						
 						if(map[i][j].energy >= tree.age) {
 							map[i][j].energy -= tree.age;
 							tree.age++;
-							map[i][j].tmpQ.add(tree);
-							if(tree.age % 5 == 0) {
-								for (int k = 0; k < pos.length; k++) {
-									int nr = tree.r + pos[k][0];
-									int nc = tree.c + pos[k][1];
-									
-									if(posCheck(nr, nc)) {
-										map[nr][nc].tmpQ.add(new Tree(nr, nc, 1));
-									}
-								}
-							}
 						}else {
 							deadMap[i][j] += (int) (tree.age / 2);
+							iterator.remove();
 						}
 					}
 				}
@@ -111,15 +102,25 @@ public class 나무재테크_16235 {
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
 					map[i][j].energy += deadMap[i][j];
+					deadMap[i][j] = 0;
 				}
 			}
 			
 			//가을
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
-					while (!map[i][j].tmpQ.isEmpty()) {
-						Tree tree = map[i][j].tmpQ.poll();
-						map[i][j].treeQueue.add(tree);
+					for (Iterator iterator = map[i][j].treeList.iterator(); iterator.hasNext();) {
+						Tree tree = (Tree) iterator.next();
+						if(tree.age % 5 == 0) {
+							for (int k = 0; k < pos.length; k++) {
+								int nr = tree.r + pos[k][0];
+								int nc = tree.c + pos[k][1];
+								
+								if(posCheck(nr, nc)) {
+									map[nr][nc].treeList.addFirst(new Tree(nr, nc, 1));
+								}
+							}
+						}
 					}
 				}
 			}
@@ -135,7 +136,7 @@ public class 나무재테크_16235 {
 		int ans = 0;
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
-				ans += map[i][j].treeQueue.size();
+				ans += map[i][j].treeList.size();
 			}
 		}
 		
